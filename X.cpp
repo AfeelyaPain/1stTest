35bc84e8606fab511137d05da8c60d06065e034a
Afeelya Pain <chazykage@gmail.com>
2018-06-23 15:51:40 +0530

    init and checkin X

---
+++ X.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class TYPE>
+void ptwice(TYPE value)
+{
+    cout << "value x2 = " << value*2 << endl;
+}
+
+//template <class T>
+/*void xtwice(T val)
+{
+    //cout << "a x 2 " << val.a*2 << endl;
+    //cout << "b = " << val.b  <<endl;
+}*/
+
+class X
+{
+    public:
+        int a;
+        char* b;
+    public:
+        X(){
+            a =0;
+            b=NULL;
+        }
+        X(int _a, char* _b)
+        {
+            a = _a;
+            b = _b;
+        }
+};
